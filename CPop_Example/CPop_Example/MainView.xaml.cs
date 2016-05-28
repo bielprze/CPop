6ca112388d2abde9a24ff7c01af64b7dc34208a3
Federico Calvagna <calvagna.federico@aajtech.com>
2016-05-27 23:33:12 -0300

    Start

---
+++ CPop_Example/CPop_Example/MainView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CPop_Example
+{
+    public partial class MainView : ContentPage
+    {   
+
+        public String Text { get; set; }
+
+        public ContentView Dialog { get; set; }
+        public Boolean ShowDialog { get; set; }
+        public Color BackColor { get; set; }
+        public Color BorderColor { get; set; }
+
+        public MainView()
+        {
+            InitializeComponent();
+
+            btnShow.Clicked += BtnShow_Clicked;
+
+            Text = "This is the main view";
+
+            var popup = new PopupView();
+            popup.Complete += Popup_Complete;
+
+            Dialog = popup;
+            BackColor = Color.Green;
+            BorderColor = Color.Red;
+
+            BindingContext = this;
+        }
+
+        private void Popup_Complete(object sender, PopupView.PopupEventArgs e)
+        {
+            Text = $"Popup result: {e.Result}";
+            ShowDialog = false;
+
+            OnPropertyChanged("ShowDialog");
+            OnPropertyChanged("Text");
+        }
+
+        private void BtnShow_Clicked(object sender, EventArgs e)
+        {
+            ShowDialog = true;
+            OnPropertyChanged("ShowDialog");
+        }
+    }
+}
---
+++ CPop_Example/CPop_Example/PopupView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CPop_Example
+{
+    public partial class PopupView : ContentView
+    {
+
+        public event EventHandler<PopupEventArgs> Complete;
+        public PopupView()
+        {
+            InitializeComponent();
+
+            btnClose.Clicked += BtnClose_Clicked;
+        }
+
+        private void BtnClose_Clicked(object sender, EventArgs e)
+        {
+            Complete?.Invoke(this, new PopupEventArgs { Result = "Hello" });
+        }
+
+        public class PopupEventArgs : EventArgs
+        {
+            public String Result { get; internal set; }
+        }
+    }
+}
